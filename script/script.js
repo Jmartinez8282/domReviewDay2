0e03227639d1bb148fcc748e5bb9585d56617d20
Code <code@COCTL-41256.local>
2019-12-03 10:10:21 -0800

    cp

--- script/script.js
+++ script/script.js
@@ -2,4 +2,11 @@ let rowNum = document.getElementById('rowNum');
 let colNum = document.getElementById('colNum');
 let pTagNum = document.getElementById('pTagNum');
 let goBtn = document.getElementById('goBtn');
+let elementArea = document.getElementById('elementArea');
+
+goBtn.addEventListener('click', function(){
+
+    
+
+});
 
