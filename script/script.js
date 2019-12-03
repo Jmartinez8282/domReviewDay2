ed97f4fc71962c47bf1719d24e0bb2df818bb811
Code <code@COCTL-41256.local>
2019-12-03 11:34:41 -0800

    cp

--- script/script.js
+++ script/script.js
@@ -5,8 +5,17 @@ let goBtn = document.getElementById('goBtn');
 let elementArea = document.getElementById('elementArea');
 
 goBtn.addEventListener('click', function(){
+    
+        console.log(rowNum.value);
 
-    elementArea.innerValue="rowNum";
-
+        for( let i=0;i<rowNum.value;i++){
+            let row = document.createElement('div');
+            row.setAttribute('class','row');
+            
+            elementArea.appendChild(row);
+        }
+   
+        
+    
 });
 
